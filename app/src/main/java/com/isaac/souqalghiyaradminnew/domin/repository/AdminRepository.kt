040578057a7d12ea8cac86c1702be3183d751ad2
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-07-07 17:11:07 +0300

    Update AdminRepository.kt

--- app/src/main/java/com/isaac/souqalghiyaradminnew/domin/repository/AdminRepository.kt
+++ app/src/main/java/com/isaac/souqalghiyaradminnew/domin/repository/AdminRepository.kt
@@ -4,9 +4,9 @@ import com.isaac.souqalghiyaradminnew.domain.model.UserEmp
 import kotlinx.coroutines.flow.Flow
 
 interface AdminRepository {
-    // دالة تسجيل الدخول (تم إضافة كلمة المرور)
     suspend fun loginAdmin(phoneNumber: String, password: String): UserEmp?
-
-    // دالة مراقبة حساب الموظف لحظياً
     fun observeAdminProfile(userId: String): Flow<UserEmp?>
+    
+    // دالة جديدة لتحديث التوكن الخاص بالموظف
+    suspend fun updateFcmToken(userId: String, token: String): Result<Unit>
 }
