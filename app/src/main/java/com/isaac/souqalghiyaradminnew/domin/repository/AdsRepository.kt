ff700182eb882b17ca2be7908b1d29f0cb32f497
ishaqamjid-jpg <ishaq.amjid@gmail.com>
2026-08-20 20:24:28 +0300

    Update AdsRepository.kt

--- app/src/main/java/com/isaac/souqalghiyaradminnew/domin/repository/AdsRepository.kt
+++ app/src/main/java/com/isaac/souqalghiyaradminnew/domin/repository/AdsRepository.kt
@@ -1,11 +1,18 @@
 package com.isaac.souqalghiyaradminnew.domain.repository
 
 import com.isaac.souqalghiyaradminnew.domain.model.Ad
+import com.isaac.souqalghiyaradminnew.domain.model.PublicAdvertisement
 import kotlinx.coroutines.flow.Flow
 
 interface AdsRepository {
+    // الإعلانات التجارية
     fun getAds(): Flow<List<Ad>>
     suspend fun addAd(ad: Ad): Result<Unit>
     suspend fun updateAd(ad: Ad): Result<Unit>
     suspend fun deleteAd(adId: String): Result<Unit>
+
+    // الإعلانات العامة (الإشعارات)
+    fun getPublicAds(): Flow<List<PublicAdvertisement>>
+    suspend fun addPublicAd(ad: PublicAdvertisement): Result<Unit>
+    suspend fun deletePublicAd(docId: String): Result<Unit>
 }
